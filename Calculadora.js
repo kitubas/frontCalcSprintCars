fffc7a043856136252240d0dc23b4f6afc2cb5ba
kitubas <henrique_leite1@yahoo.com.br>
2021-09-14 17:38:23 -0300

    new js file

---
+++ Calculadora.js
@@ -0,0 +1,10 @@
+function procurarNome(){
+    let nomessss = document.getElementById('inputModelo')
+    let nomeCarro = String(nomessss.value)
+
+    if(nomeCarro==="kwid"){    
+        resposta.innerHTML=(`bar do gordo`)
+    
+}
+}
+
