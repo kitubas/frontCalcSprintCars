d91c5f539b13e70ba3f720678eab0415e6606462
Antonio Alves <antoniocunha9610@gmail.com>
2021-09-14 20:29:01 -0300

    alteração da seção principal

--- Calculadora.js
+++ Calculadora.js
@@ -1,10 +1,18 @@
-function procurarNome(){
+//calculadora sprintcar
+//Autores:Henrique Leite e Antonio Alves
+
+function procurarNome() {
     let nomessss = document.getElementById('inputModelo')
     let nomeCarro = String(nomessss.value)
 
-    if(nomeCarro==="kwid"){    
-        resposta.innerHTML=(`bar do gordo`)
-    
-}
+    if (nomeCarro.length === 0) {
+        window.alert('escolha um modelo de carro!');
+    }
+    else {
+        if (nomeCarro === "kwid") {
+            resposta.innerHTML = (`bar do gordo`);
+
+        }
+    }
 }
 
