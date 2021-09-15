3a04f05ab94f3b77d4edbea380b51c563c76a938
kitubas <henrique_leite1@yahoo.com.br>
2021-09-15 16:02:09 -0300

    new image folder and load images function

--- Calculadora.js
+++ Calculadora.js
@@ -2,20 +2,53 @@
 //Autores:Henrique Leite e Antonio Alves
 
 function procurarNome() {
-    let nomessss = document.getElementById('inputModelo')
-    let nomeCarro = String(nomessss.value)
-    let img=document.createElement('img')
-
+    let nomessss = document.getElementById('inputModelo');
+    let nomeCarro = String(nomessss.value);
+    let img=document.createElement('img');
+    img.setAttribute('id', 'foto');
+    resposta.innerHTML='';
+      
+  
 
     if (nomeCarro.length === 0) {
         window.alert('escolha um modelo de carro!');
     }
     else {
         if (nomeCarro === "kwid") {
-            resposta.innerHTML = (`bar do gordo`);
-            img.setAttribute()
+            
+            
 
+            img.setAttribute('src' , 'img/kwid.png');
+
+           
         }
+       
+    
+        if (nomeCarro === "renegade") {
+
+            img.setAttribute('src' , 'img/renegade.png');
+
+        
+        }
+      
+    
+        if (nomeCarro === "uno") {
+
+            
+            img.setAttribute('src' , 'img/uno.png');
+
+        
+        }
+       
+    
+        if (nomeCarro === "camaro") {
+
+            img.setAttribute('src' , 'img/camaro.png');
+
+        
+        }
+        resposta.appendChild(img);
+
     }
 }
 
